0ea10467e251b3853a4aa64697935abf7499f75b
yuuki08noah <yuuki08noah@gmail.com>
2025-02-26 07:42:38 +0900

    CI/CD Pipeline Description. -> /describe

--- main.py
+++ main.py
@@ -13,3 +13,7 @@ def fuck():
 @app.get("/wow")
 def wow():
     return "해냇다 시바!!!"
+
+@app.get("/describe")
+def dec():
+    return "There are two github repositories that manage this test project, one for code and other the for the helm chart. Jenkins observes the code repository and builds docker image with timestamp tag. It also updates helm manifest to the new image tag. \nArgoCD observes the helm repository and deploys the newly updated helm manifest to kubernetes environment."
